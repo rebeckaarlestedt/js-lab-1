fa93d9a1f4cb0384bc3738b41be98842a140d161
Rebecka Arlestedt <rebecka.arlestedt@gmail.com>
2019-10-14 13:23:32 +0200

    nyare test

--- functions.ts
+++ functions.ts
@@ -2,15 +2,17 @@
 
 
 function printDetails(person) {
-  if (!person.name) return 'you forgot the name';
+  if (!person.name) return 'why are you not updating git?';
 
 
   const age = person.age ? person.age : 0;
   const country = person.country ? person.country : 'Ghana';
 
-  return `Hello ${person.name} from ${country} with age ${age} years`;
+  return `Hihihi ${person.name} from ${country} with age ${age} years`;
   console.log('You made it here boss!!!');
 }
 
 const message = printDetails({name: 'Rebecka', age: '33', country: 'Sweden' });
 console.log(message);
+
+let hejsvejs = 'hej'; 
